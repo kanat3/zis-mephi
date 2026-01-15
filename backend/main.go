8548fedab98551188a3dc613e0d3c64c887f597f
Anna Kuchebo <>
2026-01-15 14:27:01 +0300

    fix cors

--- backend/main.go
+++ backend/main.go
@@ -1336,6 +1336,7 @@ func main() {
 
 	r.HandleFunc("/requirements", getRequirementsHandler).Methods("GET")
 
+	corsRouter := corsMiddleware(r)
 	log.Println("Server starting on port 8080...")
-	log.Fatal(http.ListenAndServe(":8080", r))
+	log.Fatal(http.ListenAndServe(":8080", corsRouter))
 }
